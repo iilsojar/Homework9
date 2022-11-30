e3b688289944d12efc0460bcd8cb6ff7d334edcb
Svetlana <iilsojar@gmail.com>
2022-11-30 13:48:59 +0100

    Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.

---
+++ ex64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int M = 1; 
+
+void EvenNumbersfromMtoN (int m, int n)
+{
+    if (n == m)
+    {
+        //Console.Write("{0, 4}", n);
+        return;
+    } 
+    if (n % 2 == 0)
+    {
+        Console.Write("{0, 4}", n);
+    }
+EvenNumbersfromMtoN(m, n - 1);
+}
+
+EvenNumbersfromMtoN(M, N);
+Console.WriteLine();
+
+
+
+
